15801f16c5393bedc35d09254b098303f72ec1ee
Omar Tuffaha <omar_tuffaha@hotmail.com>
2017-06-14 15:53:16 +0300

    add the config file and database class file

---
+++ includes/Database.php
@@ -0,0 +1,137 @@
+<?php
+
+include_once 'config.php';
+
+/**
+ * @ClassName : MySql Database Class
+ * @Description : This Class is Used to perform actions in Mysql DB
+ * @Version : 2.6v
+ * @LastEdit : 18/Mar/2017
+ * @Author : Omar Tuffaha <omar_tuffaha@hotmail.com>
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ */
+class Database {
+
+    private $conn;
+    private $lastQuery;
+    private $recordSet = array();
+
+    //this is a contructer of the class datbase
+    public function __construct() {
+
+        //it start the connection with the database
+        $this->openConnection();
+    }
+
+//end of construct
+    //this function establishes a connection to database
+    public function openConnection() {
+
+        //connect to database
+        $this->conn = mysqli_connect(DBSERVER, DBUSER, DBPASS, DBNAME);
+
+        //if connection did not establish the website will die (stop)
+        if (!$this->conn) {
+            die("Cannot Connect to Server: " . mysqli_connect_error());
+        }//end of if statment
+
+        if (!mysqli_set_charset($this->conn, "utf8")) {
+            die("Error loading character set utf8: " . mysqli_connect_error() . "\n");
+            exit();
+        }
+    }
+
+//end of openConnection()
+    //this function performs the query that was requested by other classes
+    //it takes a String of the query as a paremeter
+    public function performQuery($query) {
+
+        //perform query
+        $this->lastQuery = mysqli_query($this->conn, $query);
+
+        // if the query is performed it will return the result else it
+        // will return false
+        return $this->lastQuery ? $this->lastQuery : FALSE;
+    }
+
+//end of function performQuery($query)
+    //this function performs multi insert querys that was requested by other classes
+    //it takes a String of the insert querys as a paremeter
+    public function insertMultiQuery($querys) {
+
+        //perform query
+        $this->lastQuery = mysqli_multi_query($this->conn, $querys);
+
+        // if the query is performed it will return the result else it
+        // will return false
+        return $this->lastQuery ? $this->lastQuery : FALSE;
+    }
+
+//end of performMultiQuery()
+    //this function returns all of a list that was requested from a database
+    public function fetchAll() {
+
+        $this->recordSet = NULL;
+        //this loop goes over all the items that are requested
+        while ($row = mysqli_fetch_assoc($this->lastQuery)) {
+            $this->recordSet[] = $row;
+        }//end of while loop
+        //if the recordset is not empty the record set the result else it
+        // will return false
+        return !empty($this->recordSet) ? $this->recordSet : FALSE;
+    }
+
+    //end of function fetchAll
+    //this function returns the last id used
+    public function lastInsertedId() {
+        return mysqli_insert_id($this->conn);
+    }
+
+//end of lastInserted Id
+
+    public static function mysql_escape_mimic($inp) {
+        if (is_array($inp)) {
+            return array_map(__METHOD__, $inp);
+        }
+        if (!empty($inp) && is_string($inp)) {
+            return str_replace(array('\\', "\0", "\n", "\r", "'", '"', "\x1a"), array('\\\\', '\\0', '\\n', '\\r', "\\'", '\\"', '\\Z'), $inp);
+        }
+        return $inp;
+    }
+
+    public function run_mysql_real_escape_string($string) {
+        $string = Database::encodeIllegalCharAndFilter($string);
+        return mysqli_escape_string($this->conn, $string);
+    }
+
+     public static function encodeIllegalCharAndFilter($string) {
+
+        $string = str_replace("'", "%21", $string);
+        //$string = str_replace("-", "%22", $string);
+        //$string = str_replace(".", "%23", $string);
+        $string = str_replace("\"", "%24", $string);
+        $string = str_replace("\\", "%25", $string);
+        //$string = str_replace("/", "%26", $string);
+        //$string = str_replace(" ", "%27", $string);
+        return $string;
+    }
+
+    public static function decodeIllegalChar($string) {
+
+        $string = str_replace("%21", "'", $string);
+        //$string = str_replace("%22", "-", $string);
+        //$string = str_replace("%23", ".", $string);
+        $string = str_replace("%24", "\"", $string);
+        $string = str_replace("%25","\\", $string);
+        //$string = str_replace("%26","/", $string);
+        //$string = str_replace("%27"," ", $string);
+        return $string;
+    }
+
+    public function getMysqliError() {
+        return mysqli_error($this->conn);
+    }
+
+}
+
+//end of class database
---
+++ includes/config.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * This file is set the website configeration
+ */
+
+define("DBSERVER","localhost");
+define("DBUSER","phpmyadmin");
+define("DBPASS","password");
+define("DBNAME","cms");
+
+?>
+
