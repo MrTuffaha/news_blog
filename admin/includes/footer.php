abf25a60bddd3ba171131f4ff401607e4681ee45
Omar Tuffaha <omar_tuffaha@hotmail.com>
2017-06-30 18:59:50 +0300

    -fix a mistake in add_post.php file -add WYSIWYG to better edit text in text area

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -28,7 +28,8 @@ crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.flash.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.print.min.js"></script>
 
-
+<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+<script>tinymce.init({selector: 'textarea'});</script>
 
 <script>
 
@@ -54,9 +55,10 @@ crossorigin="anonymous"></script>
                     exportOptions: {
                         columns: ':visible'
                     }
-                },'colvis'
+                }, 'colvis'
 
             ]});
+
     });
 </script>
 </body>
